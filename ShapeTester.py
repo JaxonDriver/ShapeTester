ab3407dc76638ea231ebf5c18d322f09155f975e
JaxonDriver <31314439+JaxonDriver@users.noreply.github.com>
2018-02-13 08:31:23 -0700

    Add files via upload

---
+++ ShapeTester.py
@@ -0,0 +1,15 @@
+import box, sphere, pyramid
+
+shape = input("Pick a shape to calculate: Box - b, Sphere - s, or Pyramid - p:)
+
+if shape = 'b':
+              b1 = box.Box()
+              b1.length = int(input("Enter the length of the box:"))
+              b1.width = int(input("Enter the width of the box:"))
+              b1.height = int(input("Enter the height of the box:"))
+              b1.calcVol()
+              # print(b1.calcSa())
+elif shape == 's':
+              #implementation
+elif shape == 'p':
+              #implementation
