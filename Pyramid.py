fb78efbeda90a0d95bb23b8ce25ae3b11a86a468
JaxonDriver <31314439+JaxonDriver@users.noreply.github.com>
2018-02-13 08:34:01 -0700

    Add files via upload

---
+++ Pyramid.py
@@ -0,0 +1,16 @@
+class Pyramid(object):
+    vol = 0
+    sArea = 0
+
+    def __init__(self):
+        self.length = 0
+        self.width = 0
+        self.height = 0
+
+    def calcVol(self):
+        vol = self.length * self.width * self.height
+        print (vol)
+
+    def calcSA(self):
+        sArea = self.length
+        print(surfArea)
